7253577337beadfc751325ff8826157c0e8dba25
Manish Yadav <manish.yadav.elit@gmail.com>
2024-08-25 12:14:22 +0530

    removed pwa

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
-const isExport = process.env.NEXT_PHASE === 'phase-export';
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: isExport || process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
-
+// next.config.js without PWA
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: isExport ? undefined : 'export',
+  output: "export",
   compiler: {
-    removeConsole: process.env.NODE_ENV !== 'development',
+    removeConsole: process.env.NODE_ENV !== "development",
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
