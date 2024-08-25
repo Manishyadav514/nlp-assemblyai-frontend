13c85bbd22e3dcf2cc9fc548225dada78ac61ae5
Manish Yadav <manish.yadav.elit@gmail.com>
2024-08-25 11:51:10 +0530

    ci for something

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-import withPWA from "next-pwa";
-
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public", // PWA assets directory
+  disable: process.env.NODE_ENV === "development",
+  register: true, // next-pwa registers the service worker
+  skipWaiting: true,
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -10,11 +15,4 @@ const nextConfig = {
   },
 };
 
-const pwaConfig = withPWA({
-  dest: "public", // PWA assets dir
-  disable: process.env.NODE_ENV === "development",
-  register: true, // next-pwa registers the service worker
-  skipWaiting: true,
-});
-
-export default pwaConfig(nextConfig);
+module.exports = withPWA(nextConfig);
